d10a90a40d081ae3483716a262fd222bf5cdbb11
Arun Muthukumarasamy <marunsct@gmail.com>
2020-01-22 08:03:58 +0000

    bugs

--- Components/NumberContainer.js
+++ Components/NumberContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, StyleProp } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default class NumberContainer extends Component {
   constructor(props) {

99415a40fd683cfd5c90a302fe419f2477b01b1c
Arun Muthukumarasamy <marunsct@gmail.com>
2020-01-23 06:03:33 +0000

    Bug

--- Components/NumberContainer.js
+++ Components/NumberContainer.js
@@ -8,7 +8,7 @@ export default class NumberContainer extends Component {
   render() {
     return (
       <View style={styles.NumberContainer}>
-        <Text>"hello there"</Text>
+        <Text>{props.text}</Text>
       </View>
     );
   }
@@ -23,6 +23,7 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 1 },
     shadowOpacity: 0.3,
     shadowRadius: 5,
-    elevation: 5
+    elevation: 5,
+    marginBottom: 5
   }
 });
